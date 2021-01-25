e3dfc57a23b2704daf6f244c01e03bf00cf84e3d
asus <asus@LAPTOP-URBGL9J1>
2021-01-25 23:49:52 +0600

    new

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class QuoteCategory(models.Model):
+    title = models.CharField(max_length=50)
+
+class Quote(models.Model):
+    quote = models.TextField()
+    author = models.CharField(max_length=200)
+
+    quote_category =models.ForeignKey(
+        'QuoteCategory', 
+        on_delete = models.CASCADE
+    )
